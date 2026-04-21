b0eda7087e46928adc59d8aef4412a0945c87688
Ankita <akmugli7@gmail.com>
2026-04-21 11:34:58 +0530

    updated my project

--- Time/time.js
+++ Time/time.js
@@ -1,20 +1,19 @@
 setInterval(() => {
-    
-let date = new Date();
-let hrs = date.getHours()
-let min = date.getMinutes()
-let sec = date.getSeconds()
 
-// console.log(`${h}Hrs: ${m}Min: ${s}Sec:`)
-if(hrs>12){
-    hrs = hrs-12
-    h.innerText = `${hrs} pm`
-    m.innerText = `${min} min`
-    s.innerText = `${sec} sec`
-}
-else{
-    h.innerText = `${hrs} am`
-    m.innerText = `${min} min`
-    s.innerText = `${sec} sec`
-}
+    let date = new Date();
+    let hrs = date.getHours()
+    let min = date.getMinutes()
+    let sec = date.getSeconds()
+
+    if (hrs > 12) {
+        hrs = hrs - 12
+        h.innerText = `${hrs} pm`
+        m.innerText = `${min} min`
+        s.innerText = `${sec} sec`
+    }
+    else {
+        h.innerText = `${hrs} am`
+        m.innerText = `${min} min`
+        s.innerText = `${sec} sec`
+    }
 }, 6000);
